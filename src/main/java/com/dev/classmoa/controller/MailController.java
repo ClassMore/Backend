0f06bac23aa41c2122f400e7a2964fae00750f97
jang gyu min <gmjang6349@gmail.com>
2023-10-18 14:57:33 +0900

    [지훈, 규민] fix: 메일 컨트롤러와 멤버 컨트롤러 분리

---
+++ src/main/java/com/dev/classmoa/controller/MailController.java
@@ -0,0 +1,39 @@
+package com.dev.classmoa.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.classmoa.dto.mail.request.CodeVerificationRequest;
+import com.dev.classmoa.dto.mail.request.EmailCheckRequest;
+import com.dev.classmoa.service.MailService;
+
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MailController {
+
+	private final MailService mailService;
+
+	@PostMapping("/email/check")
+	public ResponseEntity<Void> EmailCheck(@RequestBody EmailCheckRequest request) throws MessagingException,
+		UnsupportedEncodingException {
+		String authCode = mailService.sendEmail(request);
+		//        return authCode;
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/email/verification")
+	public ResponseEntity<Boolean> verificationEmail(@RequestBody CodeVerificationRequest request) {
+		Boolean response = mailService.verifiedCode(request);
+
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+}
